9e15bb2907dc74154a59eb49e8dd3ad60354cd23
dovgopoly <idovgopoly@gmail.com>
2023-04-27 18:30:53 +0300

    added MasterRouter tests & fixed wei utils

--- scripts/utils/utils.ts
+++ scripts/utils/utils.ts
@@ -1,11 +1,15 @@
 import { ethers } from "hardhat";
 import { BigNumber } from "ethers";
 
-export function wei(value: string, decimal: number = 18) {
-  return ethers.utils.parseUnits(value, decimal);
+export function wei(value: string | number | bigint, decimal: number = 18): bigint {
+  if (typeof value == "number" || typeof value == "bigint") {
+    value = value.toString();
+  }
+
+  return ethers.utils.parseUnits(value as string, decimal).toBigInt();
 }
 
-export function fromWei(value: string | number, decimal: number = 18) {
+export function fromWei(value: string | number | bigint, decimal: number = 18) {
   return BigNumber.from(value).div(BigNumber.from(10).pow(decimal));
 }
 
--- test/MasterRouter.test.ts
+++ test/MasterRouter.test.ts
@@ -1,75 +1,163 @@
+import { expect } from "chai";
 import { Reverter } from "./helpers/reverter";
 import { Builder, getBuilder } from "./utils/builder";
 import {
-  BridgeRouter__factory,
   MasterRouter__factory,
-  MulticallRouter__factory,
   SwapDiamond__factory,
-  TraderJoeRouter__factory,
-  TransferRouter__factory,
-  UniswapV2Router__factory,
-  UniswapV3Router__factory,
-  WETH9Mock__factory,
-  WrapRouter__factory,
   SwapDiamond,
   MasterRouter,
+  ERC721MintableBurnable,
+  ERC721MintableBurnable__factory,
+  ERC1155MintableBurnable__factory,
+  ERC1155MintableBurnable,
 } from "../generated-types/ethers";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { ethers } from "hardhat";
 import { SelectorType } from "./utils/contants";
+import { wei } from "../scripts/utils/utils";
 
-describe("SwapDiamond", async () => {
+describe("SwapDiamond", () => {
   const reverter = new Reverter();
 
   let builder: Builder;
+
+  let SwapDiamondFactory: SwapDiamond__factory;
+  let MasterRouterFactory: MasterRouter__factory;
+  let ERC721MintableBurnableFactory: ERC721MintableBurnable__factory;
+  let ERC1155MintableBurnableFactory: ERC1155MintableBurnable__factory;
 
   let OWNER: SignerWithAddress;
   let CALLER: SignerWithAddress;
 
   let diamond: SwapDiamond;
   let master: MasterRouter;
+  let erc721: ERC721MintableBurnable;
+  let erc1155: ERC1155MintableBurnable;
 
   before("setup", async () => {
     builder = await getBuilder();
 
     [OWNER, CALLER] = await ethers.getSigners();
 
-    const SwapDiamond = await ethers.getContractFactory("SwapDiamond");
-    const MasterRouter = await ethers.getContractFactory("MasterRouter");
+    SwapDiamondFactory = await ethers.getContractFactory("SwapDiamond");
+    MasterRouterFactory = await ethers.getContractFactory("MasterRouter");
+    ERC721MintableBurnableFactory = await ethers.getContractFactory("ERC721MintableBurnable");
+    ERC1155MintableBurnableFactory = await ethers.getContractFactory("ERC1155MintableBurnable");
 
-    diamond = await SwapDiamond.deploy();
-    master = await MasterRouter.deploy();
+    diamond = await SwapDiamondFactory.deploy();
+    master = await MasterRouterFactory.deploy();
 
-    await diamond["addFacet(address,bytes4[],uint8[])"](
-      master.address,
-      [
-        "0x00000000",
-        builder("make").selector,
-        builder("onERC721Received").selector,
-        builder("onERC1155Received").selector,
-        builder("onERC1155BatchReceived").selector,
-      ],
-      [
-        SelectorType.SwapDiamond,
-        SelectorType.SwapDiamond,
-        SelectorType.SwapDiamond,
-        SelectorType.SwapDiamond,
-        SelectorType.SwapDiamond,
-      ]
-    );
+    erc721 = await ERC721MintableBurnableFactory.deploy("ERC721Mock", "ERC721Mock", OWNER.address, "");
+    await erc721.mintTo(OWNER.address, 1, "");
 
-    master = await MasterRouter.attach(diamond.address);
+    erc1155 = await ERC1155MintableBurnableFactory.deploy(OWNER.address, "");
+    await erc1155.mintTo(OWNER.address, 1, 1, "");
+    await erc1155.mintTo(OWNER.address, 2, 2, "");
 
     await reverter.snapshot();
   });
 
   afterEach(reverter.revert);
 
-  describe("#onERC721Received", function () {});
+  describe("#receive", () => {
+    it("should not receive if selector is not added", async () => {
+      const tx = OWNER.sendTransaction({
+        to: diamond.address,
+        value: wei("1"),
+      });
 
-  describe("#onERC721Received", function () {});
+      await expect(tx).to.be.revertedWith("Diamond: selector is not registered");
+    });
 
-  describe("#onERC721Received", function () {});
+    it("should receive if selector is added", async () => {
+      await diamond["addFacet(address,bytes4[],uint8[])"](master.address, ["0x00000000"], [SelectorType.SwapDiamond]);
 
-  describe("#supportsInterface", function () {});
+      const tx = OWNER.sendTransaction({
+        to: diamond.address,
+        value: wei("1"),
+      });
+
+      await expect(tx).to.changeEtherBalances([OWNER, diamond], [-wei("1"), wei("1")]);
+    });
+  });
+
+  describe("#onERC721Received", () => {
+    it("should not safeTransferFrom if selector is not added", async () => {
+      const tx = erc721["safeTransferFrom(address,address,uint256)"](OWNER.address, diamond.address, 1);
+
+      await expect(tx).to.be.revertedWith("Diamond: selector is not registered");
+    });
+
+    it("should not safeTransferFrom with data if selector is not added", async () => {
+      const tx = erc721["safeTransferFrom(address,address,uint256,bytes)"](OWNER.address, diamond.address, 1, "0x1337");
+
+      await expect(tx).to.be.revertedWith("Diamond: selector is not registered");
+    });
+
+    context("selector is added", () => {
+      beforeEach(async () => {
+        await diamond["addFacet(address,bytes4[],uint8[])"](
+          master.address,
+          [builder("onERC721Received").selector],
+          [SelectorType.SwapDiamond]
+        );
+      });
+
+      it("should safeTransferFrom if selector is added", async () => {
+        const tx = erc721["safeTransferFrom(address,address,uint256)"](OWNER.address, diamond.address, 1);
+
+        await expect(tx).to.changeTokenBalances(erc721, [OWNER, diamond], [-1, 1]);
+      });
+
+      it("should safeTransferFrom with data if selector is added", async () => {
+        const tx = erc721["safeTransferFrom(address,address,uint256)"](OWNER.address, diamond.address, 1);
+
+        await expect(tx).to.changeTokenBalances(erc721, [OWNER, diamond], [-1, 1]);
+      });
+    });
+  });
+
+  describe("#onERC1155Received", () => {
+    it("should not safeTransferFrom if selector is not added", async () => {
+      const tx = erc1155.safeTransferFrom(OWNER.address, diamond.address, 1, 1, "0x");
+
+      await expect(tx).to.be.revertedWith("Diamond: selector is not registered");
+    });
+
+    it("should safeTransferFrom if selector is added", async () => {
+      await diamond["addFacet(address,bytes4[],uint8[])"](
+        master.address,
+        [builder("onERC1155Received").selector],
+        [SelectorType.SwapDiamond]
+      );
+
+      await erc1155.safeTransferFrom(OWNER.address, diamond.address, 1, 1, "0x");
+
+      expect(await erc1155.balanceOf(OWNER.address, 1)).to.eq(0n);
+      expect(await erc1155.balanceOf(diamond.address, 1)).to.eq(1n);
+    });
+  });
+
+  describe("#onERC1155BatchReceived", () => {
+    it("should not safeBatchTransferFrom if selector is not added", async () => {
+      const tx = erc1155.safeBatchTransferFrom(OWNER.address, diamond.address, [1, 2], [1, 2], "0x");
+
+      await expect(tx).to.be.revertedWith("Diamond: selector is not registered");
+    });
+
+    it("should safeBatchTransferFrom if selector is added", async () => {
+      await diamond["addFacet(address,bytes4[],uint8[])"](
+        master.address,
+        [builder("onERC1155BatchReceived").selector],
+        [SelectorType.SwapDiamond]
+      );
+
+      await erc1155.safeBatchTransferFrom(OWNER.address, diamond.address, [1, 2], [1, 2], "0x");
+
+      expect(await erc1155.balanceOf(OWNER.address, 1)).to.eq(0);
+      expect(await erc1155.balanceOf(OWNER.address, 2)).to.eq(0);
+      expect(await erc1155.balanceOf(diamond.address, 1)).to.eq(1);
+      expect(await erc1155.balanceOf(diamond.address, 2)).to.eq(2);
+    });
+  });
 });
